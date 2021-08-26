9278fe4dfc29515f7245c181e8ec387c84930e25
Alexander Gansior <gansior-ag@rosseti.digital>
2021-08-26 12:20:31 +0300

    tempfile libreris

--- interesting_libraries/turtle/first_proba.py
+++ interesting_libraries/turtle/first_proba.py
@@ -3,16 +3,24 @@ import turtle
 def sqrt_col():
     pass
 
-cursor = turtle.Turtle()
-cursor.speed(20)
+wndow = turtle.Screen()# создаем окно
+wndow.title("Screen & Button")
+wndow.setup(width=500, height=500)# устанавливаем размер окна
+wndow.colormode(255) # устанавливаем что цвета назначать бувем RGB
+cursor = turtle.Turtle() # создаем курсор для рисования
+cursor.speed(20) # скорость рисования
 
-for _ in range(4):
-    cursor.forward(50)
-    cursor.left(90)# повернуть на лево на 90 градусов
-cursor.up()# поднять курсор что бы не оставлял след
-cursor.setposition(-250, -200)
-cursor.down()
-cursor.color('red')
-cursor.hideturtle()# делаем сам курсор невидимым
+for k in range(10,0,-1):
+    for d in range(10,0,-1):
+        cursor.setposition(-250+k*40+25, -250+d*40+25) # устанавливаем курсор в соответствующую позицию экрана
+        cursor.color(150+d*10,k*20,k*20) # рассчитываем цвет заливки
+        cursor.begin_fill() # начало заливки
+        cursor.circle(20) # нарисовать круг
+        cursor.end_fill() # конец заливки
+
+#cursor.up()# поднять курсор что бы не оставлял след
+#cursor.down()
+
+
 
 turtle.mainloop()
