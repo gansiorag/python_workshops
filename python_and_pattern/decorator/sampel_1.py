e00edb5f2c7a9315041eae65560f365d5b8577c1
gansiorag <gansiorag@it.mos.ru>
2023-12-11 12:37:09 +0300

    add ch

---
+++ python_and_pattern/decorator/sampel_1.py
@@ -0,0 +1,18 @@
+def decor_one(func):
+    def inner():
+        print()
+        print('inner')
+        print()
+        ex_func = 'AAAAAA !!!!!   ' + func()
+        return print(ex_func)
+    return inner
+
+
+@decor_one
+def print_hello():
+    print("Hello!!!!")
+    return 'Alex!!!'
+
+
+print_hello()
+print('End!!!')
