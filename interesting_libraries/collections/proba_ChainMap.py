b7feea742023adcd524f94a75d07aeca85e34a3d
gansiorag <gansiorag@it.mos.ru>
2023-12-11 12:02:56 +0300

    add ch

--- interesting_libraries/collections/proba_ChainMap.py
+++ interesting_libraries/collections/proba_ChainMap.py
@@ -4,11 +4,15 @@
 Athor: Gansior Alexander, gansior@gansior.ru, +79173383804
 Starting 2022//
 Ending 2022//
-    
+
 '''
-    
+
+from collections import ChainMap
+import sys
+import os
 from termcolor import cprint
 import inspect
+
 '''
 Text colors: grey red green yellow blue magenta cyan white
 Text highlights: on_grey on_red on_green on_yellow on_blue on_magenta on_cyan on_white
@@ -19,18 +23,15 @@ template --> cprint(f'{}' , 'red', attrs=['bold'])
 Shows which module is currently running
 cprint('='*20 + ' >> ' + inspect.stack()[0][0].f_code.co_name + ' << '+'='*20, 'red', attrs=['bold'])
 '''
-    
-import os
-import sys
- 
+
+
 nameProjectStart = 'python_workshops'
 cprint(os.getcwd(), 'green')
-PathPrj = os.getcwd().split(nameProjectStart)[0] + nameProjectStart +  '/'
+PathPrj = os.getcwd().split(nameProjectStart)[0] + nameProjectStart + '/'
 cprint(PathPrj, 'blue')
 sys.path.append(PathPrj)
-    
-from collections import ChainMap
-    
+
+
 def prog1():
     baseline = {'music': 'bach', 'art': 'rembrandt'}
     adjustments = {'art': 'van gogh', 'opera': 'carmen'}
@@ -38,13 +39,13 @@ def prog1():
     dd1 = list(ChainMap(adjustments))
     cprint(dd, 'red')
     cprint(dd1, 'green')
-    
-    
+
+
 def prog2():
     pass
-    
-    
+
+
 if __name__ == '__main__':
-    name=''
+    name = ''
     prog1()
     prog2()
