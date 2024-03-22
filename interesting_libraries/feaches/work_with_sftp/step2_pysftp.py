86c68438f684e51de377cbfd123683d16b311132
gansiorag <gansiorag@it.mos.ru>
2024-03-22 16:25:03 +0300

    add ch

--- interesting_libraries/feaches/work_with_sftp/step2_pysftp.py
+++ interesting_libraries/feaches/work_with_sftp/step2_pysftp.py
@@ -3,9 +3,9 @@ import pysftp
 import paramiko
 
 host = 'localhost'
-# username = 'gansiorag'
-username = 'root'
-password = 'gansiorag'
+username = 'gansiorag'
+# username = 'root'
+password = 'GANsiBR5668899!'
 # password = ''
 
 
@@ -30,11 +30,27 @@ class My_Connection(pysftp.Connection):
             self.close()
 
 
-srv = My_Connection(host=host, username=username, private_key='sftp_one.txt')
+# srv = My_Connection(host=host, username=username, private_key=password)
 # srv.chdir('/home/test')  # change directory on remote server
 # srv.put('sftp_one.txt')  # To download a file, replace put with get
-lc = srv.listdir()
-print(lc)
-dd = srv.get('sftp_one.txt')
-print(dd)
-srv.close()
+# lc = srv.listdir()
+# print(lc)
+# # dd = srv.put('/home/gansiorag/sftp_one.txt')
+# dd = srv.getcwd()
+# print(dd)
+# srv.close()
+
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+
+with pysftp.Connection(host=host,
+                       username=username,
+                       password=password,
+                       cnopts=cnopts) as sftp:
+    print("Connection succesfully stablished")
+    sftp.cwd('/home/gansiorag/')  # Switch to a remote directory
+    f = sftp.open('new_filename.txt', 'w')
+    f.write('proba sdgsgds sdfhsdfh sdhdhf string')
+    # directory_structure = sftp.listdir_attr()
+    # for attr in directory_structure:
+    #     print(attr.filename, attr)
