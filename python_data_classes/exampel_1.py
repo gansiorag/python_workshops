b705a697d39cdc214f325577656e4760ccf5f498
gansiorag <gansior@gansior.ru>
2024-04-13 05:20:53 +0300

    add ch

--- python_data_classes/exampel_1.py
+++ python_data_classes/exampel_1.py
@@ -1,10 +1,10 @@
+from dataclasses import dataclass
+
+
 class RegularBook:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-
-
-from dataclasses import dataclass
 
 
 @dataclass
