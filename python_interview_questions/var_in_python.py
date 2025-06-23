1524ae754e36e409e571e5eabd1ac96764416451
gansiorag <breslau1962@yandex.ru>
2025-06-23 09:02:09 +0300

    фвв ср

---
+++ python_interview_questions/var_in_python.py
@@ -0,0 +1,18 @@
+import inspect
+
+
+def f1():
+    global x
+    x = 10
+    print(x)
+    x = 20
+    print(x)
+    frame = inspect.currentframe()
+    print()
+    print(frame.f_back.f_locals)
+    print()
+    print(frame)
+
+if __name__ == "__main__":
+    print('__name__ == ', __name__)
+    f1()
