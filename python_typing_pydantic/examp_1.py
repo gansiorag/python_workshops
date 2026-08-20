51906368a5fe61523d1fa26197aaee3745d8e31a
gansior <breslau1962@yandex.ru>
2026-08-20 20:36:57 +0300

    add ch

--- python_typing_pydantic/examp_1.py
+++ python_typing_pydantic/examp_1.py
@@ -3,34 +3,41 @@ import functools
 from typing import Callable, TypeVar, ParamSpec
 
 # 1. Задаем переменные для типов
-P = ParamSpec("P")    # Для параметров (аргументов)
-R = TypeVar("R")      # Для возвращаемого результата (Return)
+P = ParamSpec("P")  # Для параметров (аргументов)
+R = TypeVar("R")  # Для возвращаемого результата (Return)
+
 
 def timer(func: Callable[P, R]) -> Callable[P, R]:
     """Декоратор, который печатает время выполнения функции."""
-    
+
     @functools.wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         print(func.__name__)
+        print(dir(func))
         start_time = time.perf_counter()
-        
+
         # Выполняем саму функцию
         result = func(*args, **kwargs)
-        
+
         end_time = time.perf_counter()
-        print(f"--- Функция '{func.__name__}' выполнилась за {end_time - start_time:.4f} сек. ---")
-        
+        print(
+            f"--- Функция '{func.__name__}' выполнилась за {end_time - start_time:.4f} сек. ---"
+        )
+
         return result
-        
+
     return wrapper
 
+
 # --- Применяем декоратор ---
+
 
 @timer
 def heavy_computation(x: int, name: str = "Robot") -> str:
     """Имитируем сложную работу."""
     time.sleep(1.5)
     return f"Результат для {name}: {x * 2}"
+
 
 # Теперь проверь в своем редакторе:
 # Наведи курсор на 'res' — он покажет тип 'str' (благодаря TypeVar R).
